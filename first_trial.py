3a80c10ff9e64759b56c500cb584705af33bc3a9
HemchandraPatil <88281942+HemchandraPatil@users.noreply.github.com>
2024-03-13 18:33:42 +0000

    okay

--- first_trial.py
+++ first_trial.py
@@ -6,23 +6,40 @@ import snowflake.connector
 from urllib.error import URLError
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
-#from ydata_profiling import pandas_profiling as pf
-#from pydantic_settings import BaseSettings
 
-# if 'layout_preference' not in st.session_state:
-#     st.session_state['layout_preference'] = 'wide'
-#     st.set_page_config(page_title="Web Application", page_icon=":bulb:", layout=st.session_state['layout_preference'])
+st.set_page_config(
+    page_title="Saama's Global Hackthon",
+    page_icon=":wave:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
 
 def create_session():
     return Session.builder.configs(st.secrets["snowflake"]).create()
-
 session = create_session()
 
-#st.image('saama_logo.png', caption = "Saama's Logo")
+col1, col2,col3 = st.columns(3)
+
+with col1:
+    st.image('saama_logo.jpg',width = 150)
+
+with col2:
+    st.header("Saama Thunder's")
+
+with col3:
+    st.header("Hackathon 2024")
+
+st.markdown("""<hr style="height:2px;border:none;color:#1E90FF;background-color:#1E90FF;" /> """, unsafe_allow_html=True)
 # Write directly to the app
-st.title("Welcome to Streamlit in Snowflake")
-st.header("Saama Thunder's") 
-status = st.radio("Select One of the Stage: ", ('Internal Satge', 'External Stage(S3)','Named Stage'))
+st.header("Simple Data Management Application using Snowflake, Streamlit, SnowPark")
+
+with st.container(border=True):
+    status = st.radio("Select One Stage: ", ('Internal Satge', 'External Stage(S3)','Named Stage'),index=None)
 
 # conditional statement to print 
 if (status == 'Internal Satge'):
@@ -46,30 +63,17 @@ if (status == 'Named Stage'):
 
 if(status=='External Stage(S3)'):
     #session = get_active_session()
-
     st.subheader("External Stage Files:")
-    
     sql = "LIST @STORE_DB.ATLAS.AWS_S3_STG;"
-    
     df = session.sql(sql).collect()
-    
     st.dataframe(data=df,use_container_width=True)
 
-    #from pathlib import Path
-
-    #file_path = df[0]['name']
-    #st.success(Path(file_path).name)
-    
     #files = st.selectbox("Please select any one file from the below list",(df))
-    
+
     sql_filename = "select distinct METADATA$FILENAME from @STORE_DB.ATLAS.AWS_S3_STG"
-
     df_filename = session.sql(sql_filename).collect()
-
     files = st.selectbox("Please select any one file from the below list",(df_filename))
-    #st.write("You select",files)
-    
-    #st.button("Preview",type="primary")
+
     #Write directly to the app
     if 'clicked' not in st.session_state:
         st.session_state.clicked = False
@@ -82,7 +86,7 @@ if(status=='External Stage(S3)'):
     #Preview code
     if st.session_state.clicked:
         # The message and nested widget will remain on the page
-        st.write("You have selected",files)
+        st.write(f"You have selected {files} :")
         #extract the table name from file name
         table_name = files.replace(' ', '')[:-4].upper()
 
@@ -93,31 +97,49 @@ if(status=='External Stage(S3)'):
         session.sql(sql_copy).collect()
         sql_select = f"select * from STORE_DB.ATLAS.{table_name}"
         df_sql_select = session.sql(sql_select).collect()
-        #st.write(df_sql_select)
-        dataframe_select = st.dataframe(df_sql_select)
-        record_slider = st.sidebar.slider("How many records you wanted to see :",0,100,10)
+        dataframe_select = pd.DataFrame(df_sql_select)
+        #st.write(dataframe_select)
+
+        # Show data
+        @st.cache_data(show_spinner=False)
+        def split_frame(input_df, rows):
+            df3 = [input_df.loc[i : i + rows - 1, :] for i in range(0, len(input_df), rows)]
+            return df3
+        
+        pagination = st.container()
+
+        bottom_menu = st.columns((4,1,1))
+        with bottom_menu[2]:
+            batch_size = st.selectbox("Page Size", options=[10, 20, 50])
+        with bottom_menu[1]:
+            total_pages = (
+                            int(len(dataframe_select) / batch_size) if int(len(dataframe_select) / batch_size) > 0 else 1
+                        )
+            current_page = st.number_input(
+                                            "Page", min_value=1, max_value=total_pages, step=1
+                                        )
+        with bottom_menu[0]:
+            st.markdown(f"Page **{current_page}** of **{total_pages}** ")
+
+        pages = split_frame(dataframe_select, batch_size)
+        pagination.dataframe(data=pages[current_page - 1], use_container_width=True)
 
         #Column Names
         sql_columns = f"select column_name from information_schema.columns where table_name = '{table_name}' and table_schema = 'ATLAS';"
         df_sql_columns = session.sql(sql_columns).collect()
         df_columns = pd.DataFrame(df_sql_columns)
 
-        #For practice purpose
         st.markdown('----------------------')
-
-        
-        #Multiselect
+        #Data Profiling
         st.subheader("Data Profiling")
-        st.caption('Please select any columns from the sidebar :')
-        #@st.cache
-        #column_names = st.multiselect('Please select any column:',df_sql_columns)
-        #st.write("Selected Columns : ",column_names)
-#         if 'Onclicked' not in st.session_state:
-#             st.session_state.Onclicked = False
+        st.caption("Data Profiling Table :")
+        #code to show data profiling
+        desc = dataframe_select.describe(include="all")
+        st.write(desc.transpose())
 
         #Checkbox
         #Function to create checkbox
-        #st.cache_data.clear()
+        st.caption('Please select any columns from the sidebar :')
         def checkbox_container(data):
             #select_column_box = st.text_input('Please select any column')
             cols = st.columns(5)
@@ -143,51 +165,53 @@ if(status=='External Stage(S3)'):
 
         selected_column = get_selected_checkboxes()
 
-        for values in selected_column:
-            #sql query to preview the data for selected columns
-            # to show the distinct records
-            #values2 = f"values.append({values})"
-            select_sql = f"select distinct {values} from {table_name}"
-            collect_select_sql = session.sql(select_sql).collect()
-            df_select_sql = pd.DataFrame(collect_select_sql)
-            st.write(df_select_sql)
-
-            #sf_select_sql_final = sf_select_sql_final || df_select_sql
-
-            # report = pf.ProfileReport(df_select_sql)
-            # st.write(report)
-        
-        select_sql = f"select {values},count(*) as Total from {table_name} group by {values}"
-        collect_select_sql = session.sql(select_sql).collect()
-        df_select_sql = pd.DataFrame(collect_select_sql)
-        st.write(df_select_sql)
-        
-        # x_col = df_select_sql['CITY']
-        # y_col = df_select_sql['TOTAL']
-        # chart_data = st.bar_chart(df_select_sql, x= x_col[1],y=y_col)
-        # st.write(chart_data)
-
-        col1, col2 = st.columns(2)
+        #to preview the data for selected columns
         try:
-            with col1:
-                st.header("Distinct Record")
-                select_sql = f"select distinct {values} from {table_name}"
+            appended_data = []
+            for values in selected_column:
+                #sql query to preview the data for selected columns
+                # to show the distinct records
+                select_sql = f"select distinct {values} from {table_name} limit 5"
                 collect_select_sql = session.sql(select_sql).collect()
                 df_select_sql = pd.DataFrame(collect_select_sql)
-                st.write(df_select_sql)
-
-            with col2:
-                st.header("Counts")
-                count_sql = f"select {values},count(*) as Total from {table_name} group by {values}"
-                collect_count_sql = session.sql(count_sql).collect()
-                df_count_sql = pd.DataFrame(collect_count_sql)
-                st.write(df_count_sql)
+                appended_data.append(df_select_sql) 
+            df2 = pd.concat(appended_data, axis=1,ignore_index=True)
+            df2.columns = selected_column
+            st.write(df2)
         except:
-            st.write("No columns selected")  
+            st.write("Please select any one column from the sidebar checkbox")
 
+         
+        # col1, col2 = st.columns(2)
+        # try:
+        #     with col1:
+        #         st.header("Distinct Record")
+        #         select_sql = f"select distinct {values} from {table_name}"
+        #         collect_select_sql = session.sql(select_sql).collect()
+        #         df_select_sql = pd.DataFrame(collect_select_sql)
+        #         st.write(df_select_sql)
+
+        #     with col2:
+        #         st.header("Counts")
+        #         count_sql = f"select {values},count(*) as Total from {table_name} group by {values}"
+        #         collect_count_sql = session.sql(count_sql).collect()
+        #         df_count_sql = pd.DataFrame(collect_count_sql)
+        #         st.write(df_count_sql)
+        # except:
+        #     st.write("No columns selected")  
+
+        # null_sql = f"select 'Null count' as NULL_COUNT, (select count(*) from mocked_speaker_data where upper({values}) = 'NAN') as COUNT"
+        # collect_null_sql = session.sql(null_sql).collect()
+        # df_null_sql = pd.DataFrame(collect_null_sql)
+        # st.write(collect_null_sql)
+
+        #query for dtype
+        # desc = dataframe_select.describe(include="all")
+        # st.write(desc.transpose())
+
+        edited_df = st.data_editor(dataframe_select)
+            
         
-            
-            
 
 
         
@@ -196,12 +220,6 @@ if(status=='External Stage(S3)'):
         
 
         
-# # 
-#         def click_button1():
-#             st.session_state.Onclicked = True
-# # 
-#         st.button('SUBMIT', on_click=click_button1, type = 'primary')
-#         if st.session_state.Onclicked:
-#             st.write("Selected Columns : ",column_names)
+
 
 #st.session_state.clicked = False 
